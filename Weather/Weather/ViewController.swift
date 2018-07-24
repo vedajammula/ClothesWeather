af7c1f791a695b3c3f2a23d23cd5edc4b9df7929
VedaJ <veda.jammula@gmail.com>
2018-07-24 13:02:13 -0700

    Added xcode proj

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by veda jammula on 7/24/18.
+//  Copyright © 2018 Veda Jammula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
