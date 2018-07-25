d50c8300b53c3c4f10a71c908bb32754a4517aa1
VedaJ <veda.jammula@gmail.com>
2018-07-25 13:18:47 -0700

    Created PickerView

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -8,8 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return priorityTypes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return priorityTypes[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedPriority = priorityTypes[row]
+        textfield.text = selectedPriority
+    }
 
+    var selectedPriority : String?
+    
+    var priorityTypes = ["Shirt", "Sweater" , "Shorts", "Jeans", "Dress", "Jewlrey", "Shoes", "Other"]
+    
+    
+    func createPickerView() {
+        
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        textfield.inputView = pickerView
+        
+    }
+    
+    
+    func dismissPickerView() {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissKeyboard))
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        textfield.inputAccessoryView = toolBar
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    
+    @IBOutlet weak var textfield: UITextField!
     @IBOutlet weak var myImageView: UIImageView!
     @IBAction func takePhoto(_ sender: Any) {
         let image = UIImagePickerController()
@@ -37,14 +84,11 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
         self.dismiss(animated: true, completion: nil)
     }
-        
-        
-        
-        
-        
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createPickerView()
+        dismissPickerView()
     }
 
     override func didReceiveMemoryWarning() {
