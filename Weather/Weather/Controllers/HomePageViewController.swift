41d4dc074932d3a8bd29911d37ff3b1b455ff7fc
VedaJ <veda.jammula@gmail.com>
2018-07-30 15:18:30 -0700

    deleting images works

--- Weather/Weather/Controllers/HomePageViewController.swift
+++ Weather/Weather/Controllers/HomePageViewController.swift
@@ -17,37 +17,38 @@ class HomePageViewController: UIViewController {
         }
     }
 
+    var editModeOn: Bool = false
     
     // MARK: - IBOutlets
     
+    @IBOutlet weak var addButtonBarItem: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
     
     // MARK: - View Life Cycle Methods
    
     override func viewDidLoad() {
-        
-        imageObjects = CoreDataHelper.retrieveImages()
-
         super.viewDidLoad()
-        // Fix CoreDataHelper
-        // Follow notes app to retrieve and save data
-        // Remove lines below
-
+        imageObjects = CoreDataHelper.retrieveImages()
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+    }
+    //MARK: - Delete Items
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        addButtonBarItem.isEnabled = !editing
+        editModeOn = !editModeOn
+        collectionView.reloadData()
+//        if let indexPaths = collectionView?.indexPathsForVisibleItems {
+//            for indexPath in indexPaths {
+//                if let cell = collectionView?.cellForItem(at: indexPath) as! ImageCollectionViewCell? {
+//                    cell.isEditing = editing
+//                }
+//            }
+//        }
     }
     
     // MARK: - IBActions
-
-//    func saveImage(imageName: String){
-//        //create an instance of the FileManager
-//        let fileManager = FileManager.default
-//        //get the image path
-//        let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
-//        //get the image we took with camera
-//        let image = imageView.image!
-//        //get the PNG data for this image
-//        let data = UIImagePNGRepresentation(image)
-//        //store it in the document directory    fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
-//    }
 }
 
 // MARK: - CollectionView Delegate & DataSource Methods
@@ -59,12 +60,29 @@ extension HomePageViewController: UICollectionViewDelegate, UICollectionViewData
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ImageCollectionViewCell
+        cell.isEditing = self.editModeOn
         cell.imageView.image = UIImage(data: imageObjects[indexPath.row].image!)
-        
+        cell.indexPath = indexPath
+        cell.delegate = self
         return cell
     }
    
 
+}
+
+extension HomePageViewController : ImageCollectionViewCellDelegate {
+    func delete(indexPath: IndexPath) {
+        imageObjects.remove(at: indexPath.row)
+        collectionView.reloadData()
+    }
+    
+//    func delete(cell: ImageCollectionViewCell) {
+//        if let indexPath = collectionView?.indexPath(for: cell) {
+//       //     photoCategories[indexPath.section].imageNames
+//
+//            collectionView?.deleteItems(at: [indexPath])
+//        }
+//    }
 }
 
 
--- Weather/Weather/Views/ImageCollectionViewCell.swift
+++ Weather/Weather/Views/ImageCollectionViewCell.swift
@@ -9,9 +9,42 @@
 import Foundation
 import UIKit
 
+protocol ImageCollectionViewCellDelegate: class {
+    
+    func delete (indexPath: IndexPath)
+    
+}
+
+
 class ImageCollectionViewCell: UICollectionViewCell {
     
+    var indexPath: IndexPath?
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    
+    
+    @IBAction func deleteButtonDidTap(_ sender: Any) {
+        
+        delegate?.delete(indexPath: indexPath!)
+    }
+    @IBOutlet weak var deleteButtonBackgroundView: UIVisualEffectView!
     @IBOutlet weak var imageView: UIImageView!
     
+    weak var delegate: ImageCollectionViewCellDelegate?
+    
+    var imageName: String! {
+        didSet{
+            deleteButtonBackgroundView.layer.cornerRadius = deleteButtonBackgroundView.bounds.width / 2.0
+            deleteButtonBackgroundView.layer.masksToBounds = true
+            deleteButtonBackgroundView.isHidden = !isEditing
+        }
+    }
+    var isEditing: Bool = false {
+        didSet {
+            deleteButtonBackgroundView.isHidden = !isEditing
+            
+        }
+    }
     
 }
