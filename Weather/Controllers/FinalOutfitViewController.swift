4409ffa0a1c81ab2080a844f813c92efd05ddc19
VedaJ <veda.jammula@gmail.com>
2018-08-02 14:35:46 -0700

    Started Login/Registration pages

---
+++ Weather/Controllers/FinalOutfitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FinalOutfitViewController.swift
+//  Weather
+//
+//  Created by veda jammula on 8/2/18.
+//  Copyright © 2018 Veda Jammula. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FinalOutfitViewController: UIViewController {
+    
+    
+}
---
+++ Weather/Controllers/RegisterPageViewController.swift
@@ -0,0 +1,59 @@
+////
+////  RegisterPageViewController.swift
+////  Weather
+////
+////  Created by veda jammula on 8/2/18.
+////  Copyright © 2018 Veda Jammula. All rights reserved.
+////
+//
+//import UIKit
+//
+//class RegisterPageViewController: UIViewController {
+//
+////    @IBOutlet weak var repeatPasswordTextField: UITextField!
+////    @IBOutlet weak var userPasswordTextField: UITextField!
+////    @IBOutlet weak var userEmailTextField: UITextField!
+////    override func viewDidLoad() {
+////        super.viewDidLoad()
+////
+////        // Do any additional setup after loading the view.
+////    }
+////
+////    override func didReceiveMemoryWarning() {
+////        super.didReceiveMemoryWarning()
+////        // Dispose of any resources that can be recreated.
+////    }
+////
+////    @IBAction func registerButtonTapped(_ sender: Any) {
+////
+////        let userEmail = userEmailTextField.text
+////        let userPassword = userPasswordTextField.text
+////        let userRepeatPassword = repeatPasswordTextField.text
+////
+////
+////        if (userEmail?.isEmpty)! || (userPassword?.isEmpty)! || (userRepeatPassword?.isEmpty)!{
+////
+////            displayMyAlertMessage(userMessage:"All fields are required")
+////
+////            return
+////        }
+////
+////        if userPassword != userRepeatPassword {
+////            displayMyAlertMessage(userMessage: "Fields do not match!")
+////            return
+////        }
+////
+////    }
+////
+////
+////    func displayMyAlertMessage(userMessage: String) {
+////        var myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertController)
+////    }
+////
+////
+//
+//
+//    /*
+//    // MARK: - Navigation
+//
+// }
